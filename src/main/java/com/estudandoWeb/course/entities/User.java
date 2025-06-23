4a4fbc4495d80113675729e278c433ea8bed180e
Gustavo Antunes Veiga do Carmo <gustavo.carmo@estudante.ifto.edu.br>
2025-06-23 17:36:31 -0300

    H2 database, teste profile, JPA

--- src/main/java/com/estudandoWeb/course/entities/User.java
+++ src/main/java/com/estudandoWeb/course/entities/User.java
@@ -1,19 +1,24 @@
 package com.estudandoWeb.course.entities;
 
-import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
-@Serial
-    private static final long serialVersionUID = 1L;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
+
+    public User() {
+    }
 
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
@@ -22,6 +27,7 @@ public class User implements Serializable {
         this.phone = phone;
         this.password = password;
     }
+
 
     public Long getId() {
         return id;
